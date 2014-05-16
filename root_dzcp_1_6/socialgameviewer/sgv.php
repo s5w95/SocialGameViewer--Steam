5d321b8cf64d759fa406e827ef7cc4acdb2e4ecd
Sascha Wäschle <sascha.waeschle@d4ho.de>
2014-05-16 21:31:57 +0200

    Fix Error

--- root_dzcp_1_6/socialgameviewer/sgv.php
+++ root_dzcp_1_6/socialgameviewer/sgv.php
@@ -61,7 +61,7 @@ function getListFrom($playerid) {
     if ($settings->view_addfriend) {
 		$x_size = $settings->x_size*0.88;
 		$width = $settings->x_size*0.12;
-		$add = '<img src="http://steamsignature.com/AddFriend.png" width="'.$width.'"/>'
+		$add = '<img src="http://steamsignature.com/AddFriend.png" width="'.$width.'"/>';
 	} else {
 		$x_size = $settings->x_size;
 	}
