50f013d06cb36bbe0496463c7c9452a6bb296027
Sascha Wäschle <sascha.waeschle@d4ho.de>
2015-12-05 23:40:34 +0100

    Add empty cache folder
    - Add SymLink in builds

--- cms_builds/dzcp/root_dzcp_1_6/socialgameviewer/lib/Socialgameviewer/module/selfrenderimage/selfrenderimage.php
+++
@@ -1,6 +0,0 @@
-<?php
-function render($player, $settings) {
-    return '<a href="'.$player['profile_url'].'">'
-                .'<img src="'.dirname($_SERVER['PHP_SELF']).'/render/'.$player['comid'].'" width="'.$settings->x_size.'" height="'.$settings->x_size*0.2.'" title="Profile from '.$player['comid'].'"/>'
-            .'</a>';
-}
