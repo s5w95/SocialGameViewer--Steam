6ffcb689b879144baecde5834a409b65b403e914
Sascha Wäschle <sascha.waeschle@d4ho.de>
2015-12-06 00:03:25 +0100

    Fix ln

---
+++ cms_builds/dzcp/root_dzcp_1_6/socialgameviewer/lib/Socialgameviewer/module/steamsignature_com/steamsignature_com.php
@@ -0,0 +1,13 @@
+<?php
+	function render($player, $settings) {
+        $add = "";
+		if ($settings->view_addfriend) {
+			$x_size = $settings->x_size*0.88;
+			$width = $settings->x_size*0.12;
+			$add = '<img src="http://steamsignature.com/AddFriend.png" width="'.$width.'"/>';
+		} else {
+			$x_size = $settings->x_size;
+		}
+
+		return '<a href="'.$player['profile_url'].'"><img src="http://steamsignature.com/status/english/'.$player['comid'].'.png" width="'.$x_size.'"/></a>'.$add;
+	}
